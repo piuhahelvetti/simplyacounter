cf25f43a259b166fdb07d2c374f7b98670b332dc
averynova <93867381+averynova@users.noreply.github.com>
2022-09-14 18:22:06 +0300

    Migrate to GitHub

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+# Counter
+A simple counter using the state React-hook.
+
+## ToDo
+
+- Add the ability to start from custom amount
+- Add the ability to create a custom step amount
---
+++ src/App.js
@@ -0,0 +1,72 @@
+import './App.css';
+import { useState } from "react";
+
+export default function App() {
+
+  const [count, setCount] = useState(0);
+
+  function plusOne(){
+    setCount(count + 1);
+  } //plusOne
+
+  function minusOne (){
+    setCount (count - 1);  
+  } //plusOne
+
+  function plusFive (){
+    setCount(count + 5);
+  } //plusFive
+
+  function minusFive (){
+    setCount(count -5);
+  } //plusFive
+
+  function plusTen(){
+    setCount(count +10);
+  } //plusFiTen
+
+  function minusTen(){
+    setCount(count -10);
+  }//minusTen
+
+  function reset(){
+    var ConfirmReset = confirm ("Are you sure you want to reset the counter?");
+    if (ConfirmReset == true) {
+    setCount(0);
+    } else {
+      setCount(count);
+  }//reset(else)
+  }//reset
+
+  return (
+    <div className="App">
+      <h1>Counter</h1>
+
+      <h2 id="count">{count}</h2>
+    
+    <div className='plusMinusOne'>
+
+      <button id="plusone" onClick={plusOne}>+</button>
+
+      <button id="minusone" onClick={minusOne}>-</button> <br/>
+
+    </div>
+
+    <div className='plusMinusFive'>
+
+      <button id="plusfive" onClick={plusFive}>+5</button>  
+
+      <button id="minusfive" onClick={minusFive}>-5</button>    
+
+    </div>
+
+    <div className="plusMinusTen">
+      <button id="plusten" onClick={plusTen}>+10</button>
+      <button id="minusten" onClick={minusTen}>-10</button>
+    </div>
+
+      <button id="reset" onClick={reset}>Reset</button>
+
+    </div>
+  );// return
+}//App
