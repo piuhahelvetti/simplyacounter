320cfa6e5e89a6eb99dafd2ff7c20ba77bbc3c50
Leevi Saastamoinen <93867381+piuhahelvetti@users.noreply.github.com>
2023-03-21 06:50:15 +0000

    Localstorage step values + reset steps & reset all

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import "./App.css";
 import { useState } from "react";
 export default function App():JSX.Element {
-	const [plusStep, setPlusStep] = useState(1);
-
-	const [minusStep, setMinusStep] = useState(1);
 
 	const prevCount:number = parseInt(localStorage.getItem("count"));
+	const prevPlusStep:number = parseInt(localStorage.getItem("plusStep"));
+	const prevMinusStep:number = parseInt(localStorage.getItem("minusStep"));
 
+	//Checks if local storage value is NaN (Case in new browsers) and sets initial state accordingly
+	const [count, setCount] = useState(!isNaN(prevCount)? prevCount:0);
 
-	const [count, setCount] = useState(!isNaN(prevCount)? prevCount:0); //Checks if local storage value is NaN (Case in new browsers,  and sets initial state accordingly)
+	const [plusStep, setPlusStep] = useState(!isNaN(prevPlusStep)?prevPlusStep:1);
+
+	const [minusStep, setMinusStep] = useState(!isNaN(prevMinusStep)?prevMinusStep:1);
 
 	window.onbeforeunload = function (event) {
 		if(count !== 0){
@@ -16,51 +19,49 @@ export default function App():JSX.Element {
 	}
 };
 
-	function updateLocalStorage (newCount:number) {
-		localStorage.setItem("count", JSON.stringify(newCount));
+	function updateLocalStorage (item:string, value:any ) {
+		localStorage.setItem(item, JSON.stringify(value));
 	}
 
 	function plus() {
 		setCount(count + plusStep);
 		console.log("Added" + plusStep);
-		updateLocalStorage(count+plusStep);
+		updateLocalStorage("count", count+plusStep);
 	} //plusOne
 
 	function minus() {
 		setCount(count - minusStep);
 		console.log("Substracted" + minusStep);
-
-		updateLocalStorage(count-minusStep);
+		updateLocalStorage("count", count-minusStep);
 	} //plusOne
 
 	function plusFive() {
 		setCount(count + 5);
 		console.log("Added 5");
-		updateLocalStorage(count+5);
+		updateLocalStorage("count", count+5);
 
 	} //plusFive
 
 	function minusFive() {
 		setCount(count - 5);
 		console.log("Substracted 5");
-    
-		updateLocalStorage(count-5);
+		updateLocalStorage("count", count-5);
 
 	} //plusFive
 
 	function plusTen() {
 		setCount(count + 10);
 		console.log("Added 10");
-		updateLocalStorage(count+10);
+		updateLocalStorage("count", count+10);
 	} //plusFiTen
 
 	function minusTen() {
 		setCount(count - 10);
 		console.log("Added 10");
-		updateLocalStorage(count-10);
+		updateLocalStorage("count", count-10);
 	} //minusTen
 
-	function reset() {
+	function resetCount() {
 		if (count !== 0) {
 			const confirmReset = window.confirm(
 				"Are you sure you want to reset the counter?"
@@ -68,7 +69,7 @@ export default function App():JSX.Element {
 			if (confirmReset === true) {
 				setCount(0);
 				console.log("Counter has been reset");
-				updateLocalStorage(0);
+				updateLocalStorage("count", 0);
 			} else {
 				setCount(count);
 			} //setcount else
@@ -84,7 +85,7 @@ export default function App():JSX.Element {
 			setCount(count)
 		} else {
 			setCount(customStart);
-			updateLocalStorage(customStart);
+			updateLocalStorage("count", customStart);
 			console.log(
 				"Custom starting point set. Counter value is now " + customStart
 			);
@@ -95,6 +96,7 @@ export default function App():JSX.Element {
 		let customPlusStepInput = Number(prompt("Enter desired + step amount"));
 		if (!isNaN(customPlusStepInput)) {
 			setPlusStep(customPlusStepInput);
+			updateLocalStorage("plusStep", customPlusStepInput		);
 			console.log("Plus step set to " + plusStep);
 		} else {
 			setPlusStep(1);
@@ -105,11 +107,44 @@ export default function App():JSX.Element {
 		let customMinusStepInput = Number(prompt("Enter desired - step amount"));
 		if (!isNaN(customMinusStepInput)) {
 			setMinusStep(customMinusStepInput);
+			updateLocalStorage("minusStep", customMinusStepInput)
 			console.log("Minus step set to " + minusStep);
 		} else {
 			setMinusStep(1);
 		} //setPlusStepPrompt.else
 	} //setPlusStepPrompt
+
+function resetPlusStep(){
+		const confirmPlusStepReset = window.confirm("Are you sure you want to reset + step");
+		if (confirmPlusStepReset === true){
+			setPlusStep(1);
+			updateLocalStorage("plusStep", 1);
+	} else {
+		setPlusStep(plusStep);
+	}
+}
+
+function resetMinusStep(){
+	const confirmMinusStepReset = window.confirm("Are you sure you want to reset + step");
+	if (confirmMinusStepReset === true){
+		setMinusStep(1);
+		updateLocalStorage("minusStep", 1);
+} else {
+	setMinusStep(minusStep);
+}
+}
+
+function resetAll () {
+	const confirmResetAll = window.confirm("Are you sure you want to reset everything?");
+	if (confirmResetAll === true){
+		setCount(0)
+		setPlusStep(1)
+		setMinusStep(1)
+		updateLocalStorage("count", 0);
+		updateLocalStorage("plusStep", 1);
+		updateLocalStorage("minusStep", 1)
+	}
+}
 
 	return (
 		<div className="App">
@@ -141,7 +176,7 @@ export default function App():JSX.Element {
 					-10
 				</button>
 			</div>
-			<button id="reset" onClick={reset}>
+			<button id="reset" onClick={resetCount}>
 				Reset
 			</button>
 			<br />
@@ -153,6 +188,11 @@ export default function App():JSX.Element {
 			<button onClick={setMinusStepPrompt}>
 				Set custom - step amount
 			</button>
+			<br />
+			<button onClick={resetPlusStep}>Reset + step</button>
+			<button onClick={resetMinusStep}>Reset - step</button>
+			<br />
+			<button onClick={resetAll}>Reset everything</button>
 			<br />
 			<footer>
 				<h2>&copy; Leevi Saastamoinen 2022-{new Date().getFullYear()}</h2>
